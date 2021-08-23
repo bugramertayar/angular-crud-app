2ab5e43c064549e86c1b8c0a77f29d7332f69cd5
Buğra Ayar <bugra.ayar@vendrops.com>
2021-08-23 20:15:37 +0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {InventoriesService} from "./modules/inventory/data/services/inventories-service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [InventoriesService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
